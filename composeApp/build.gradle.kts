03c060bddb7d0b25aee145ea7d786dd801fd6bc4
himanshugaur <himanshu@spare8.com>
2024-01-06 17:00:30 +0530

    Dependency of the project is here.

---
+++ composeApp/build.gradle.kts
@@ -0,0 +1,109 @@
+import org.jetbrains.compose.ExperimentalComposeLibrary
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsCompose)
+    kotlin("plugin.serialization") version "1.9.21"
+
+}
+
+kotlin {
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+
+        androidMain.dependencies {
+            implementation(libs.compose.ui.tooling.preview)
+            implementation(libs.androidx.activity.compose)
+
+            // koin di
+            implementation("io.ktor:ktor-client-android:2.3.7")
+            implementation("io.insert-koin:koin-core:3.5.0")
+            implementation("io.insert-koin:koin-android:3.4.3")
+        }
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material)
+            implementation(compose.ui)
+            @OptIn(ExperimentalComposeLibrary::class)
+            implementation(compose.components.resources)
+
+            // kamel image loading
+            implementation("media.kamel:kamel-image:0.9.1")
+
+            // ktor client
+            implementation("io.ktor:ktor-client-core:2.3.7")
+            implementation("io.ktor:ktor-client-content-negotiation:2.3.7")
+            implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.7")
+            implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
+
+            // precompose
+            api("moe.tlaster:precompose:1.5.10")
+            api("moe.tlaster:precompose-viewmodel:1.5.10")
+            api("moe.tlaster:precompose-koin:1.5.10")
+
+            // koin
+            implementation("io.insert-koin:koin-core:3.5.0")
+            implementation("io.insert-koin:koin-compose:1.1.0")
+
+        }
+        iosMain.dependencies {
+            // darwin ktor client for ios
+            implementation("io.ktor:ktor-client-darwin:2.3.7")
+        }
+    }
+}
+
+android {
+    namespace = "com.gaur.himanshu"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    sourceSets["main"].res.srcDirs("src/androidMain/res")
+    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
+
+    defaultConfig {
+        applicationId = "com.gaur.himanshu"
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        targetSdk = libs.versions.android.targetSdk.get().toInt()
+        versionCode = 1
+        versionName = "1.0"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    dependencies {
+        debugImplementation(libs.compose.ui.tooling)
+    }
+}
+
